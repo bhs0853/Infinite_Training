8d6a95fd20195ab3e5b60ab2631de0513d436260
Hema Sai Bonda <hemasaibonda1@gmail.com>
2025-08-03 10:49:01 +0530

    WebTechnologies: Assignment 2 added

---
+++ WebTechnologies/HTML/assignments/assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+/*
+ * 1. Using HTML, CSS create display an image overlay effect on hover
+
+2. Create a CSS3 based button using style elements/properties
+
+3. Define a Descriptive/Definition List using your own items
+
+4. How to make use of animation-delay , and then restart again
+
+5. Create a Tribute Page of a person with his or her image on the top of the page. Name the person
+   Below that give his/her achievements or any information about the person.
+
+ Hint(Use paragraphs,lists,images with CSS, background color, font style etc. to make it look good.
+ */
