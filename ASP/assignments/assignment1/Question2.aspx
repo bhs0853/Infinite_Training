8dda62e078be525434facbc4652a73dc693a667b
Hema Sai Bonda <hemasaibonda1@gmail.com>
2025-08-17 13:10:11 +0530

    ASP: assignment 1 added

---
+++ ASP/assignments/assignment1/Question2.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Question2.aspx.cs" Inherits="assignment1.Question2" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Question 2</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Select a Product:</h2>
+
+        <asp:DropDownList ID="ddlProducts" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlProducts_SelectedIndexChanged" Width="260px">
+            <asp:ListItem Text="-- Select Product --" Value=""></asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <br />
+
+        <asp:Image ID="imgProduct" runat="server" Width="258px" Height="300px" />
+        <br />
+        <br />
+
+        <asp:Button ID="getPriceBtn" runat="server" Text="Get This Product Price" BackColor="#6395EE" BorderColor="#6395EE" BorderStyle="Solid" BorderWidth="2px" ForeColor="White" Width="257px" OnClick="getPriceBtn_Click" />
+        <br />
+        <br />
+
+        <asp:Label ID="lblPrice" runat="server" Font-Bold="true" ForeColor="Purple" />
+    </form>
+</body>
+</html>
---
+++ ASP/assignments/assignment1/Question2.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assignment1
+{
+    public partial class Question2 : System.Web.UI.Page
+    {
+        Dictionary<string, (string, string)> products = new Dictionary<string, (string, string)>
+        {
+            { "Hp Envy", ("~/images/envy.jpg", "₹98,000") },
+            { "Apple Macbook", ("~/images/macbook.jpg", "₹112,000") },
+            { "Asus Vivobook", ("~/images/vivobook.jpg", "₹86,500") },
+            { "Dell XPS", ("~/images/xps.jpg", "₹100,000") }
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                foreach (var product in products.Keys)
+                {
+                    ddlProducts.Items.Add(product);
+                }
+            }
+        }
+
+        protected void ddlProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (products.TryGetValue(ddlProducts.SelectedValue, out var product))
+            {
+                imgProduct.ImageUrl = product.Item1;
+                lblPrice.Text = "";
+            }
+            else
+            {
+                imgProduct.ImageUrl = "";
+                lblPrice.Text = "";
+            }
+        }
+
+        protected void getPriceBtn_Click(object sender, EventArgs e)
+        {
+            if (products.TryGetValue(ddlProducts.SelectedValue, out var product))
+            {
+                lblPrice.Text = $"Price of This Laptop: {product.Item2}";
+            }
+            else
+            {
+                lblPrice.Text = "Please select a product.";
+            }
+        }
+    }
+}
