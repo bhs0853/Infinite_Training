df223cc4ba479fc270391511f7126b56585f4972
Hema Sai Bonda <hemasaibonda1@gmail.com>
2025-09-01 11:22:45 +0530

    WebAPI: coding challenge 10 added

---
+++ WebAPI/codeChallenges/CodingChallenge10/Question1/Controllers/CountryController.cs
@@ -0,0 +1,64 @@
+﻿using Question1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Question1.Controllers
+{
+    public class CountryController : ApiController
+    {
+        static List<Country> countryList = new List<Country>()
+        {
+            new Country{ID = 1, CountryName = "India", Capital = "New Delhi"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Country> GetAllCountries()
+        {
+            return countryList;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetCountryById(int id)
+        {
+            Country country = countryList.Find(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public IEnumerable<Country> PostCountry([FromBody] Country country)
+        {
+            countryList.Add(country);
+            return countryList;
+        }
+
+        [HttpPut]
+        public Country Put(int id, [FromBody] Country country)
+        {
+            var countryById = countryList.Find(c => c.ID == id);
+
+            if (countryById == null)
+                return null;
+
+            countryById.ID = country.ID;
+            countryById.CountryName = country.CountryName;
+            countryById.Capital = country.Capital;
+            return countryById;
+        }
+
+        [HttpDelete]
+        public IEnumerable<Country> DeleteCountry(int id)
+        {
+            var countryById = countryList.Find(c => c.ID == id);
+
+            if (countryById != null)
+                countryList.Remove(countryById);
+            return countryList;
+        }
+    }
+}
---
+++ WebAPI/codeChallenges/CodingChallenge10/Question2/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Question2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Question2.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private practiceEntities2 db = new practiceEntities2();
+
+        [HttpGet]
+        [Route("getCustomersByCountry")]
+        public IHttpActionResult GetCustomersByCountry(string country)
+        {
+            var countryList = db.fn_FindCustomerByCountryName(country);
+            if (countryList == null || countryList.Count() == 0)
+                return NotFound();
+            return Ok(countryList);
+        }
+
+        [HttpGet]
+        [Route("getOrdersById")]
+        public IHttpActionResult GetOrders(int empId)
+        {
+            var orders = db.Orders.Where(o => o.EmployeeID == empId);
+            if (orders == null || orders.Count() == 0)
+                return NotFound();
+            return Ok(orders);
+        }
+
+    }
+}
